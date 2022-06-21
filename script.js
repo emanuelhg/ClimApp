5268495c2fa06bf3b94b7b10d6483dec9bfa7e84
Emanuel guzzetti <ehguzzetti@gmail.com>
2022-06-20 23:26:48 -0300

    Versión 1.0
    Primera versión con la API funcionando.

--- script.js
+++ script.js
@@ -3,6 +3,8 @@ inputPart = wrapper.querySelector('.input-part'),
 infoTxt = inputPart.querySelector('.info-txt'),
 inputField = inputPart.querySelector('input'),
 locationBtn = inputPart.querySelector('button'),
+wIcon = document.querySelector('.weather-part img'),
+arrowBack = wrapper.querySelector('header i'),
 apiKey = 'e2f07fa5296109348d9d890fa06b4f2c';
 
 let api;
@@ -23,7 +25,7 @@ locationBtn.addEventListener('click', () =>{
 
 function onSucess(position){
     const {latitude, longitude} = position.coords;
-    api = `https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=${apiKey}&units=metric`;
+    api = `https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=${apiKey}&units=metric&lang=sp`;
     fetchData();
 }
 
@@ -33,7 +35,7 @@ function onError(error){
 }
 
 function requestApi(city){
-    api = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+    api = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric&lang=sp`;
     fetchData();
 }
 
@@ -49,8 +51,37 @@ function weatherDetails(info){
         infoTxt.innerText = `${inputField.value} no es un nombre válido de ciudad`;
         infoTxt.classList.replace('pending', 'error');
     } else {
+        const city = info.name;
+        const country = info.sys.country;
+        const {description, id} = info.weather[0];
+        const {feels_like, humidity, temp} = info.main;
+
+        if(id == 800){
+            wIcon.src = "Icons/clear.svg";
+        } else if (id >= 200 && id <= 232){
+            wIcon.src = "Icons/storm.svg";
+        } else if (id >= 600 && id <= 622){
+            wIcon.src = "Icons/snow.svg";
+        } else if (id >= 701 && id <= 781){
+            wIcon.src = "Icons/haze.svg";
+        } else if (id >= 801 && id <= 804){
+            wIcon.src = "Icons/cloud.svg";
+        } else if ((id >= 300 && id <= 321) || (id >= 500 && id <= 531)){
+            wIcon.src = "Icons/rain.svg";
+        }
+
+        wrapper.querySelector('.temp .numb').innerText = Math.floor(temp);
+        wrapper.querySelector('.weather').innerText = description;
+        wrapper.querySelector('.location span').innerText = `${city}, ${country}`;
+        wrapper.querySelector('.temp .numb-2').innerText = Math.floor(feels_like);
+        wrapper.querySelector('.humidity span').innerText = `${humidity}%`;
+
         infoTxt.classList.remove('pending', 'error');
+        wrapper.classList.add('active');
         console.log(info);
     }
-    
 }
+
+arrowBack.addEventListener('click', ()=> {
+    wrapper.classList.remove('active');
+})
